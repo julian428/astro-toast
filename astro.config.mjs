c1fb707294993e096d1ebd6879f4e98f05ad9bee
Julian <julianjometzger@gmail.com>
2023-11-02 16:23:40 +0100

    Removed cva from the dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [],
 });
